4c1f4f75a0eebb4edff6621952f0fef2939cf8b8
sugarpramana <pramana.sugar@gmail.com>
2017-07-09 16:40:58 +0700

    Develop #24 Membuat Skills (.zul)

---
+++ Module-Parent/Module-Run/src/main/java/org/app/portofolio/common/menu/util/JHRMAdditionalZulPath.java
@@ -0,0 +1,27 @@
+package org.app.portofolio.common.menu.util;
+
+/**
+ * 
+ * @author JCodev
+ *
+ */
+public interface JHRMAdditionalZulPath {
+	
+	/**
+	 * 
+	 *
+	 */
+	public interface MasterData {
+		
+		/**
+		 * 
+		 *
+		 */
+		public interface Skills {
+			
+			public static final String MAIN_FORM = "/WEB-INF/pages/module_hr/master/qualifications/skills/skillList.zul";
+			public static final String ADD_FORM = "/WEB-INF/pages/module_hr/master/qualifications/skills/skillDialog.zul";
+			public static final String DETAIL_FORM = "/WEB-INF/pages/module_hr/master/qualifications/skills/skillDetailDialog.zul";
+		}
+	}
+}
