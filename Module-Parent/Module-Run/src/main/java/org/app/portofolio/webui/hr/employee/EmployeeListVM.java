515fea8b60f71e989d398d7a97dae1ded9c5bac9
amyBachtiar <amy.bachtiar@gmail.com>
2017-07-14 03:07:28 +0700

    sedikit memperbaiki dan vm dan zul untuk employee

---
+++ Module-Parent/Module-Run/src/main/java/org/app/portofolio/webui/hr/employee/EmployeeListVM.java
@@ -0,0 +1,33 @@
+package org.app.portofolio.webui.hr.employee;
+
+import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.ExecutionArgParam;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.select.Selectors;
+
+public class EmployeeListVM {
+
+	/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	 * Inisialize Methode MVVM yang pertama kali dijalankan
+	 *++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+	@AfterCompose
+	public void setupComponents(@ContextParam(ContextType.VIEW) Component component,
+		@ExecutionArgParam("object") Object object) {
+		
+		Selectors.wireComponents(component, this, false);
+	}
+	
+	@Command
+	public void doNew(){
+		Executions.createComponents("/WEB-INF/pages/module_hr/employeeDialog.zul", null, null);
+	}
+	
+	@Command
+	public void doDetail(){
+		Executions.createComponents("/WEB-INF/pages/module_hr/employeeDetailDialog.zul", null, null);
+	}
+}
