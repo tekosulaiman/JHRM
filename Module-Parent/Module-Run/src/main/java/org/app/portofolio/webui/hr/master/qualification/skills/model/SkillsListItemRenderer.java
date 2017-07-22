a8b0798db3460ca869854b37431b24da8678eba3
sugarpramana <pramana.sugar@gmail.com>
2017-07-22 13:56:28 +0700

    Perbaikan adaptasi perubahan model

--- Module-Parent/Module-Run/src/main/java/org/app/portofolio/webui/hr/master/qualification/skills/model/SkillsListItemRenderer.java
+++ Module-Parent/Module-Run/src/main/java/org/app/portofolio/webui/hr/master/qualification/skills/model/SkillsListItemRenderer.java
@@ -1,35 +1,35 @@
 package org.app.portofolio.webui.hr.master.qualification.skills.model;
 
 import org.app.portofolio.webui.hr.common.components.InlineListItemRenderer;
-import org.module.hr.model.OhrmSkill;
+import org.module.hr.model.MstSkill;
 import org.zkoss.zul.Label;
 import org.zkoss.zul.Listcell;
 import org.zkoss.zul.Listitem;
 import org.zkoss.zul.Textbox;
 
-public class SkillsListItemRenderer extends InlineListItemRenderer<OhrmSkill>{
+public class SkillsListItemRenderer extends InlineListItemRenderer<MstSkill>{
 	
 	
 	@Override
-	public void renderListItem(Listitem item, OhrmSkill value, int index) throws Exception {
+	public void renderListItem(Listitem item, MstSkill value, int index) throws Exception {
 		Textbox textboxName = new Textbox();
-		textboxName.setValue(value.getName());
+		textboxName.setValue(value.getNameSkill());
 		textboxName.setVisible(false);
 		
 		Textbox textboxDescription = new Textbox();
-		textboxDescription.setValue(value.getDescription());
+		textboxDescription.setValue(value.getDescpriptionSkill());
 		textboxDescription.setVisible(false);
 		
 		Label labelName = new Label();
-		labelName.setValue(value.getName());
+		labelName.setValue(value.getNameSkill());
 		
 		Label labelDescription = new Label();
-		labelDescription.setValue(value.getDescription());
+		labelDescription.setValue(value.getDescpriptionSkill());
 		
 		setEditingComponents(textboxName, textboxDescription);
 		setLabelingComponents(labelName, labelDescription);
 		
-		if (value.getId() == null) {
+		if (value.getIdSkill() == null) {
 			textboxName.setVisible(true);
 			textboxDescription.setVisible(true);
 			onAdding(true);
