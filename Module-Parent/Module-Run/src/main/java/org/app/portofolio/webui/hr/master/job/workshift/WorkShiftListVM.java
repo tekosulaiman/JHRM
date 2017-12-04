da987cb358af761c9a1f3777f4c215fe035ad379
tekosulaiman <tekosulaiman@yahoo.com>
2017-12-04 11:35:24 +0700

    Work Shift

---
+++ Module-Parent/Module-Run/src/main/java/org/app/portofolio/webui/hr/master/job/workshift/WorkShiftListVM.java
@@ -0,0 +1,26 @@
+package org.app.portofolio.webui.hr.master.job.workshift;
+
+import org.zkoss.bind.annotation.AfterCompose;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.ContextParam;
+import org.zkoss.bind.annotation.ContextType;
+import org.zkoss.bind.annotation.ExecutionArgParam;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.select.Selectors;
+
+public class WorkShiftListVM {
+	
+	@AfterCompose
+	public void setupComponents(@ContextParam(ContextType.VIEW) Component component, 
+		@ExecutionArgParam("object") Object object) {
+		
+		Selectors.wireComponents(component, this, false);
+
+	}
+	
+	@Command
+	public void doNew(){
+		Executions.createComponents("/WEB-INF/pages/module_hr/master/job/workshift/workShiftDialog.zul", null, null);
+	}
+}
