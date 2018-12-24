78eab9c3f530335f79cbda0504d8ae80ed20890d
Tom <yudhi.saputra@rebelworks.co>
2018-12-24 15:49:28 +0700

    Service Attendance

--- Module-Parent/Module-Hr/src/main/java/org/module/hr/service/AttendanceService.java
+++ Module-Parent/Module-Hr/src/main/java/org/module/hr/service/AttendanceService.java
@@ -1,5 +1,29 @@
 package org.module.hr.service;
 
-public interface AttendanceService {
+import java.util.HashMap;
+import java.util.List;
 
+import org.module.hr.model.MstCustomer;
+import org.module.hr.model.MstProject;
+
+public interface AttendanceService {
+	/*- Customer +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+	public void save(MstCustomer mstCustomer);
+    public void update(MstCustomer mstCustomer);
+    public void saveOrUpdate(MstCustomer mstCustomer);
+    public void delete(MstCustomer mstCustomer);
+    public List<MstCustomer> getAllMstCustomers();
+    public List<MstCustomer> getByMstCustomerRequestMap(HashMap<String, Object> hashMap);
+    public List<MstCustomer> getMstCustomerPaging(HashMap<String, Object> hashMap);
+    public int getCountMstCustomers();
+    
+    /*- Project +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+	public void save(MstProject mstProject);
+    public void update(MstProject mstProject);
+    public void saveOrUpdate(MstProject mstProject);
+    public void delete(MstProject mstProject);
+    public List<MstProject> getAllMstJobtitles();
+    public List<MstProject> getByMstProjectRequestMap(HashMap<String, Object> hashMap);
+    public List<MstProject> getMstProjectPaging(HashMap<String, Object> hashMap);
+    public int getCountMstProjects();
 }
